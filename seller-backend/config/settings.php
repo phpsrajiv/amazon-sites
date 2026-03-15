7d67736fc1d3fb7cd12bd7d5946532ba74e2bca3
Vatsal Khanna <vatsalkhanna26@gmail.com>
2026-03-15 21:32:45 +0530

    Adds settings and services file in config.

---
+++ seller-backend/config/settings.php
@@ -0,0 +1,41 @@
+<?php
+
+// phpcs:ignoreFile
+
+$databases['default']['default'] = [
+  'database' => getenv('DRUPAL_DB_NAME') ?: 'drupal',
+  'username' => getenv('DRUPAL_DB_USER') ?: 'drupal',
+  'password' => getenv('DRUPAL_DB_PASSWORD') ?: '',
+  'host' => getenv('DRUPAL_DB_HOST') ?: 'mariadb',
+  'port' => getenv('DRUPAL_DB_PORT') ?: '3306',
+  'driver' => 'mysql',
+  'prefix' => '',
+  'collation' => 'utf8mb4_general_ci',
+];
+
+$settings['hash_salt'] = getenv('DRUPAL_HASH_SALT') ?: 'default-hash-salt-change-me';
+
+$settings['update_free_access'] = FALSE;
+
+$settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
+
+$settings['file_scan_ignore_directories'] = [
+  'node_modules',
+  'bower_components',
+];
+
+$settings['entity_update_batch_size'] = 50;
+$settings['entity_update_backup'] = TRUE;
+$settings['migrate_node_migrate_type_classic'] = FALSE;
+
+// Config sync directory.
+$settings['config_sync_directory'] = '../config/sync';
+
+$settings['trusted_host_patterns'] = [
+  '^localhost$',
+  '^127\.0\.0\.1$',
+];
+
+if (getenv('DRUPAL_ENV') === 'development') {
+  $settings['trusted_host_patterns'][] = '.*';
+}
